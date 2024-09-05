f9c4c6b236ba5fceca06b34ed9bc68ab1f0674f9
abstech911 <140934693+abubakarsani-raven@users.noreply.github.com>
2024-09-05 16:39:44 +0100

    master branch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [count, setCOUNT] = useState(0);
+    const [users, setUsers] = useState([]);
+    const [selectedUser, setSelectedUser] = useState(null);
+
+
+    const handleClick = async (id) => {
+        try {
+            const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+            const user = await response.json();
+            setSelectedUser(user);
+        } catch (e) {
+
+        }
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('https://jsonplaceholder.typicode.com/users');
+                const userArray = await response.json();
+                setUsers(userArray);
+            } catch (e) {
+                console.log(`Error message : ${e.message}`);
+            }
+
+        }
+        fetchData();
+    }, []);
+
+
+    useEffect(() => {
+        console.log(selectedUser)
+    }, [selectedUser]);
+
+    return (
+        <div className="App">
+            <p>My count is: {count}</p>
+            <div>
+                <button onClick={() => setCOUNT(count + 1)}>plus</button>
+                <button onClick={() => setCOUNT(count - 1)}>minus</button>
+            </div>
+            <h1>List of Users </h1>
+            <ul>
+                {
+                    users.map((item, index) => {
+                        return <li key={`id-${item.id}`} onClick={() => handleClick(item.id)}>{item.name}</li>
+                    })
+                }
+            </ul>
+            <hr/>
+            <h4>Selected User </h4>
+            {selectedUser ? (
+                <>
+                    <p>Name :{selectedUser.name}</p>
+                    <p>Phone Number : {selectedUser.phone}</p>
+                </>
+            ) : <div>No user selected</div>
+            }
+        </div>
+    );
 }
 
 export default App;
